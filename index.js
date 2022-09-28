ba9f8e1fd81a0ef40bbd126ee304974bdf356a6e
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2022-09-27 18:30:18 -0700

    commit tree fba7a81d97e9b92901d03322dee858c113f26306

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+const writeTo = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(writeTo){
+    for (const element of writeTo){
+        console.log(`Thank you, ${element}, for the wonderful surprise gift!`)
+    }
+    return writeTo
+}
+writeCards(writeTo)
